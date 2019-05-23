f8ee1e62ead6411247e1f9ed345946a5bc306d92
Cheolkie Kim <ckiekim@naver.com>
2019-05-23 23:25:55 +0900

    JSON Example

--- jspbook/src/json/JsonArray.java
+++ jspbook/src/json/JsonArray.java
@@ -12,13 +12,11 @@ public class JsonArray {
 		
 		JSONArray jArray = new JSONArray();
 		JSONObject school = new JSONObject();
-		school.put("subject1", "math");
-		school.put("subject2", "science");
+		school.put("subject", "math");
 		jArray.add(school);
 		
 		school = new JSONObject();
-		school.put("subject1", "국어");
-		school.put("subject2", "영어");
+		school.put("subject", "국어");
 		jArray.add(school);
 		jObject.put("school", jArray);
 		
@@ -31,8 +29,7 @@ public class JsonArray {
 			System.out.println("Size= " + returnSchool.size());
 			for (int i=0; i<returnSchool.size(); i++) {
 				JSONObject returnSubject = (JSONObject) returnSchool.get(i);
-				System.out.println("subject : " + returnSubject.get("subject1"));
-				System.out.println("subject : " + returnSubject.get("subject2"));
+				System.out.println("subject : " + returnSubject.get("subject"));
 			}
 		} catch(Exception e) {
 			e.printStackTrace();
--- jspbook/src/json/JsonMIx.java
+++ jspbook/src/json/JsonMIx.java
@@ -3,7 +3,7 @@ package json;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-public class Json {
+public class JsonMIx {
 	public static void main(String[] args) {
 		// JSON DATA 는 가장 아래 단계에서 부터 만들어 나간다.
 		System.out.println("----------------------------------------------");
@@ -14,13 +14,11 @@ public class Json {
 		
 		JSONArray jArray = new JSONArray();
 		JSONObject school = new JSONObject();
-		school.put("subject1", "math");
-		//school.put("subject2", "science");
+		school.put("subject", "math");
 		jArray.add(school);
 		
 		school = new JSONObject();
-		school.put("subject1", "국어");
-		//school.put("subject2", "영어");
+		school.put("subject", "국어");
 		jArray.add(school);
 		jObject.put("school", jArray);
 		
@@ -33,8 +31,7 @@ public class Json {
 			System.out.println("Size= " + returnSchool.size());
 			for (int i=0; i<returnSchool.size(); i++) {
 				JSONObject returnSubject = (JSONObject) returnSchool.get(i);
-				System.out.println("subject : " + returnSubject.get("subject1"));
-				//System.out.println("subject : " + returnSubject.get("subject2"));
+				System.out.println("subject : " + returnSubject.get("subject"));
 			}
 			int age = (Integer) jObject.get("age");
 			String name = (String) jObject.get("name");
